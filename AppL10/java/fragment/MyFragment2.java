4a07ffca53b6413f70a1f41d5334361ae9a9c0f9
Efthymios Chondrogiannis <chondrog@mail.ntua.gr>
2025-01-18 03:55:28 +0200

    Lecture 10 source code

---
+++ AppL10/java/fragment/MyFragment2.java
@@ -0,0 +1,29 @@
+package hua.dit.mobdev.ec.appl10.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hua.dit.mobdev.ec.appl10.R;
+
+public class MyFragment2 extends Fragment {
+
+    public MyFragment2() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my2, container, false);
+    }
+
+}
